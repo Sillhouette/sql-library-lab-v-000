71d7fcd48c330323ce08feb2faa0738f8b1f7c40
Richard Melchior <raustin.melchior@gmail.com>
2018-06-13 06:10:49 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,5 +1,5 @@
 
-  INSERT INTO series (title, author_id, subgenre_id) VALUES ('Chronicles of Narnia' , 1 , 2);
+  INSERT INTO series (title, author_id, subgenre_id) VALUES ('Chronicles of Narnia' , 1 , 1);
   INSERT INTO series (title, author_id, subgenre_id) VALUES ('Harry Potter' , 2 , 2);
 
 INSERT INTO subgenres(name) VALUES
