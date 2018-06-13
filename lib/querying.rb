b7d946f19f24829ab220f32fbf3b15b1776ae074
Richard Melchior <raustin.melchior@gmail.com>
2018-06-13 05:57:01 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -6,9 +6,8 @@ def select_name_and_motto_of_char_with_longest_motto
   "SELECT name, motto FROM characters ORDER BY LENGTH(motto) DESC LIMIT 1"
 end
 
-
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT species, COUNT(species) FROM characters ORDER BY COUNT(species) ASC LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
