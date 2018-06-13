40331a532d4dc2de24d63b67376e4cc43a767102
Richard Melchior <raustin.melchior@gmail.com>
2018-06-13 06:28:23 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -19,5 +19,6 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT name, COUNT(character_books.id) from character JOIN character_books ON characters.id = character_books.character_id GROUP BY name ORDER BY COUNT(character_books.id) DESC"
 end
+CREATE TABLE character_books (id INTEGER PRIMARY KEY, book_id INTEGER, character_id INTEGER);
